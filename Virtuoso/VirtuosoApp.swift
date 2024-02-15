7426d8b4c410ed11a6b0a4034c1c6b42f20366e7
Ryan Gavin <ryan.michael.gavin@gmail.com>
2024-02-15 15:23:13 -0500

    Initial Commit

---
+++ Virtuoso/VirtuosoApp.swift
@@ -0,0 +1,21 @@
+//
+//  VirtuosoApp.swift
+//  Virtuoso
+//
+//  Created by Ryan Gavin on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct VirtuosoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
