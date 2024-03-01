aee3a8042d062ff69b46821c2b9621e5afa45aa5
Ryan Gavin <ryan.michael.gavin@gmail.com>
2024-03-01 06:58:33 -0500

    Fix the crazy rotational issues

--- Virtuoso/App/AppState.swift
+++ Virtuoso/App/AppState.swift
@@ -20,6 +20,9 @@ class AppState {
     private var leftAnchor: Entity
     private var rightAnchor: Entity
 
+    // Collision bar for the notes to hit
+    private var collisionBar = Entity()
+
     private var keys: [Entity] = []
 
     var numberOfKeys: Int = 73
@@ -42,7 +45,7 @@ class AppState {
 
     /// Preload assets when the app launches to avoid pop-in during the game.
     init() {
-        centerAnchor = Entity()
+        centerAnchor = AppState.createDebugEntity()
 
         leftAnchor = AppState.createDebugEntity()
         rightAnchor = AppState.createDebugEntity()
@@ -51,18 +54,6 @@ class AppState {
         centerAnchor.addChild(rightAnchor)
 
         spaceOrigin.addChild(centerAnchor)
-    }
-
-    private static func createDebugEntity() -> Entity {
-        if debugAnchorEntity == nil {
-            // Load the entity named "Debug Entity"
-            guard let loadedEntity = try? Entity.load(named: "Debug Anchor", in: realityKitContentBundle) else {
-                fatalError("Failed to load the Debug Anchor")
-            }
-            debugAnchorEntity = loadedEntity
-        }
-
-        return debugAnchorEntity!.clone(recursive: true)
     }
 
     func moveAnchor(translation: SIMD3<Float>) {
@@ -77,50 +68,70 @@ class AppState {
         // Move the left and right anchors a bit, and then redraw the keyboard
         leftAnchor.setPosition([-stretchFactor, 0, 0], relativeTo: leftAnchor)
         rightAnchor.setPosition([stretchFactor, 0, 0], relativeTo: rightAnchor)
-        resetKeyAnchors()
+        redrawEnvironment()
     }
 
     func captureIndexFingerPosition(chirality: HandAnchor.Chirality) {
         let fingerTip = fingertips[chirality]![.indexFingerTip]!
-        let position = fingerTip.position(relativeTo: spaceOrigin)
+        let measuredPosition = fingerTip.position(relativeTo: spaceOrigin)
 
-        var leftPosition = leftAnchor.position(relativeTo: spaceOrigin)
-        var rightPosition = rightAnchor.position(relativeTo: spaceOrigin)
+        var leftPosition = chirality == .left ? measuredPosition : leftAnchor.position(relativeTo: spaceOrigin)
+        var rightPosition = chirality == .right ? measuredPosition : rightAnchor.position(relativeTo: spaceOrigin)
 
+        // Keep the height the same, using the height of the last selected anchor
         if chirality == .left {
-            leftPosition = position
+            rightPosition.y = leftPosition.y
         } else {
-            rightPosition = position
+            leftPosition.y = rightPosition.y
         }
 
         // Angle between ignoring the y axis
         let angleBetween = atan2(leftPosition.z - rightPosition.z, leftPosition.x - rightPosition.x) - .pi
 
-        // Rotate the debug anchor along the y axis to match the angle between the left and right anchors
-        // The debug anchor should be facing forward in the direction of the piano
-        centerAnchor.transform.rotation = simd_quatf(angle: angleBetween, axis: [0, -1, 0])
-
-        // Move everything to the new position
-        // Moving the center anchor will reposition the left and right anchors
-        // so they will need to be repositioned after the center anchor is moved
+        // Move the center to the new position
         let center = (leftPosition + rightPosition) / 2
         centerAnchor.setPosition(center, relativeTo: spaceOrigin)
-        leftAnchor.setPosition(leftPosition, relativeTo: spaceOrigin)
-        rightAnchor.setPosition(rightPosition, relativeTo: spaceOrigin)
 
-        resetKeyAnchors()
+        // Rotate the center so orientation is perpendicular to the line between the left and right anchors
+        centerAnchor.transform.rotation = simd_quatf(angle: angleBetween, axis: [0, -1, 0])
+
+        // Now that we have factored in the roll of the center anchor,
+        // we can set the left and right anchors to be the same height.
+        let width = simd_length(leftPosition - rightPosition)
+
+        // The left and right positions should end up the same in world space
+        // But now everything is centered to the center anchor
+        leftAnchor.position = [-width / 2, 0, 0]
+        rightAnchor.position = [width / 2, 0, 0]
+
+        redrawEnvironment()
     }
 
-    func resetKeyAnchors() {
+    func redrawEnvironment() {
         for key in keys {
             key.removeFromParent()
         }
         keys.removeAll()
+        collisionBar.removeFromParent()
 
-        var noteIndex = noteNames.firstIndex(of: startingKey[numberOfKeys]!)!
+        let noteIndex = noteNames.firstIndex(of: startingKey[numberOfKeys]!)!
 
         // Spread all the white keys evenly between the two anchors
-        var whiteKeyWidth = (rightAnchor.position - leftAnchor.position) / Float(numberOfWhiteKeys[numberOfKeys]! - 1)
+        // We are measuring the number of gaps between keys, not the number of keys
+        // This is why it's -1 in the calculation
+        // This is a vector from the left anchor to the right anchor
+        //
+        // Without the -1, we can get the actual key width
+        //
+        // Both of these values are controlled by adjusting the anchors in the configuration menu
+        let whiteKeySpacing = (rightAnchor.position - leftAnchor.position) / Float(numberOfWhiteKeys[numberOfKeys]! - 1)
+        let keyWidth = simd_length(rightAnchor.position - leftAnchor.position) / Float(numberOfWhiteKeys[numberOfKeys]!)
+        let pianoWidth = keyWidth * Float(numberOfWhiteKeys[numberOfKeys]!)
+
+        // The key width can be used to figure out the key height, which is about 6 times the width
+        // Not sure if this holds up on micro keyboards
+        // TODO: make this adjustable in the configuration menu
+        let keyHeight = keyWidth * 6
 
         // Keep track of the last white key so we can offset black keys from it
         var lastWhiteKeyPosition = leftAnchor.position
@@ -131,7 +142,7 @@ class AppState {
 
             // White keys
             if !noteName.contains("#"), !noteName.contains("b") {
-                let position = leftAnchor.position + whiteKeyWidth * Float(whiteKeyCount)
+                let position = leftAnchor.position + whiteKeySpacing * Float(whiteKeyCount)
                 let key = AppState.createDebugEntity()
                 lastWhiteKeyPosition = position
 
@@ -149,9 +160,13 @@ class AppState {
                 // In the cluster of 3, the middle note is directly in the center of the two white keys
                 // In the cluster of 2, each is slightly offset from the center of their respective white keys
                 let blackKey = AppState.createDebugEntity()
-                var blackKeyPosition = lastWhiteKeyPosition + whiteKeyWidth / 2
+                var blackKeyPosition = lastWhiteKeyPosition + whiteKeySpacing / 2
 
                 // Set the black key back and up
+                // TODO: this should be expressed as a ratio that uses the width of the white key and the number of keys
+                // TODO: this way on a shorter scale keyboard, it will still be correct
+                // TODO: the black key is always 2/3 of the size of the white key
+                // TODO: we also need to derive the height of the white key
                 blackKeyPosition.z -= 0.07
                 blackKeyPosition.y += 0.01
 
@@ -161,21 +176,39 @@ class AppState {
             }
         }
 
-        // Create some dummy notes attached to the keys
-//        createNoteEntity(color: .systemBlue, forKey: keys[3], at: [0, 0, -0.1], length: 0.1)
-//        createNoteEntity(color: .systemBlue, forKey: keys[5], at: [0, 0, -0.1], length: 0.1)
-//        createNoteEntity(color: .systemBlue, forKey: keys[7], at: [0, 0, -0.1], length: 0.1)
-//
-//        createNoteEntity(color: .systemGreen, forKey: keys[10], at: [0, 0, -0.1], length: 0.01)
-//        createNoteEntity(color: .systemGreen, forKey: keys[12], at: [0, 0, -0.11], length: 0.01)
-//        createNoteEntity(color: .systemGreen, forKey: keys[14], at: [0, 0, -0.12], length: 0.01)
+        // Draw the bar that sits just behind the keys
+        createCollisionBarEntity(width: pianoWidth, offset: keyHeight)
+    }
+
+    func createCollisionBarEntity(width: Float, offset: Float) {
+        let box = MeshResource.generateBox(width: width, height: 0.005, depth: 0.005, cornerRadius: 0.001)
+        let material = SimpleMaterial(color: .systemBlue, isMetallic: false)
+        let entity = ModelEntity(mesh: box, materials: [material])
+        entity.components[GroundingShadowComponent.self] = GroundingShadowComponent(castsShadow: true)
+        entity.position = [0, 0, -offset]
+
+        collisionBar = entity
+        centerAnchor.addChild(entity)
     }
 
     func createNoteEntity(color: UIColor, forKey: Entity, at: SIMD3<Float>, length: Float) {
         let box = MeshResource.generateBox(width: 0.01, height: 0.01, depth: length, cornerRadius: 0.001)
         let material = SimpleMaterial(color: color, isMetallic: false)
         let entity = ModelEntity(mesh: box, materials: [material])
+        entity.components[GroundingShadowComponent.self] = GroundingShadowComponent(castsShadow: true)
         entity.position = at
         forKey.addChild(entity)
+    }
+
+    private static func createDebugEntity() -> Entity {
+        if debugAnchorEntity == nil {
+            // Load the entity named "Debug Entity"
+            guard let loadedEntity = try? Entity.load(named: "Debug Anchor", in: realityKitContentBundle) else {
+                fatalError("Failed to load the Debug Anchor")
+            }
+            debugAnchorEntity = loadedEntity
+        }
+
+        return debugAnchorEntity!.clone(recursive: true)
     }
 }
