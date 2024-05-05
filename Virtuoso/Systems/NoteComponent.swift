62375ed6e406d85a2061397e27c15724a72d11b6
Ryan Gavin <ryan.michael.gavin@gmail.com>
2024-05-05 06:51:10 -0400

    Pre system refactor

---
+++ Virtuoso/Systems/NoteComponent.swift
@@ -0,0 +1,15 @@
+//
+//  NoteComponent.swift
+//  Virtuoso
+//
+//  Created by Ryan Gavin on 5/5/24.
+//
+
+import RealityKit
+
+/**
+ Represents a musical note in the reality scene.
+ */
+struct NoteComponent: Component {
+    
+}
---
+++ Virtuoso/Systems/NoteSystem.swift
@@ -0,0 +1,18 @@
+//
+//  NoteSystem.swift
+//  Virtuoso
+//
+//  Created by Ryan Gavin on 5/4/24.
+//
+
+import RealityKit
+
+class NoteSystem: System {
+    required init(scene: Scene) {
+        // Perform required initialization or setup.
+    }
+
+    func update(context: SceneUpdateContext) {
+        // RealityKit automatically calls this every frame for every scene.
+    }
+}
